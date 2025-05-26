e2b94d9fc850b16e7979a97c3db2b74995e16681
Divvi Jayaprakash <divvijayaprakash@gmail.com>
2025-05-26 21:35:25 +0530

    Fix: Updated backend server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderroute.js";
  
  //app config
  const app=express();
- const port =4000;
+ const port =process.env.PORT || 4000;
  
  //mongodb+srv://divvijayaprakash:1692971@cluster0.b3oczah.mongodb.net/?
   //middleware
