53733d23c99a89af384310e94f0295844ffb93ee
Divvi Jayaprakash <divvijayaprakash@gmail.com>
2025-05-26 22:20:16 +0530

    Fix bcrypt deploy issue for Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,12 @@ import mongoose from "mongoose"
 const mongoURI = process.env.MONGODB_URI;
 export const connectdb = async () => {
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI);
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
     process.exit(1); // Exit the process if DB connection fails
-  }
-};
- 
+  
+};      
 
+}
